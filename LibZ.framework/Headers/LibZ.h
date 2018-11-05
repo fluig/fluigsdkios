45fa6f49f3a3375aa3bb3a9d71756f74e6d7d62f
Marcelo de Aguiar <marcelodeaguiar@Marcelos-Mac-Pro.local>
2018-11-05 13:45:30 -0800

    Libz

---
+++ LibZ.framework/Headers/LibZ.h
@@ -0,0 +1,19 @@
+//
+//  LibZ.h
+//  LibZ
+//
+//  Created by Gregory Sholl e Santos on 08/06/17.
+//  Copyright © 2017 TOTVS S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibZ.
+FOUNDATION_EXPORT double LibZVersionNumber;
+
+//! Project version string for LibZ.
+FOUNDATION_EXPORT const unsigned char LibZVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibZ/PublicHeader.h>
+
+
